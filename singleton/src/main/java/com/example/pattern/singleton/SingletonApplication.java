2ec9688854b29b793cd4eb44334f65227e6a4e9a
mustafaak <mustafa.aksu@nurd.com>
2020-07-27 10:01:44 +0300

    Singleton Pattern

--- singleton/src/main/java/com/example/pattern/singleton/SingletonApplication.java
+++ singleton/src/main/java/com/example/pattern/singleton/SingletonApplication.java
@@ -1,5 +1,6 @@
 package com.example.pattern.singleton;
 
+import com.example.pattern.singleton.service.Singleton;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,13 @@ public class SingletonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SingletonApplication.class, args);
+
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println("Singleton Exp 1 ===> " + singleton1);
+        System.out.println("Singleton Exp 2 ===> " + singleton2);
+
 	}
 
 }
